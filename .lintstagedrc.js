bb21acb46a898f58a696d1aeb19f44665abd10ef
Rafael <rafaelteicas@gmail.com>
2024-10-02 22:17:05 -0300

    plop file

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
     `npm run lint --fix . ${filenames.join(' --file')}`,
